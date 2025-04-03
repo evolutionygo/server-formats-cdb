44bfb9d71dcefd009bad90ba04c586fd27a7175d
Juan Moscoso <juan.milciadesss@gmail.com>
2025-04-03 14:51:47 -0400

    Goyo fixed

--- edison/script/c511002994.lua
+++ edison/script/c511002994.lua
@@ -5,7 +5,7 @@ function c511002994.initial_effect(c)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringc511002994(7391448,0))
+	e1:SetDescription(aux.Stringid(511002994,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
